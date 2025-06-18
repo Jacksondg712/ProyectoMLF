771e860c55067c08d818760f3582512e873d64e1
Jacksondg712 <118758501+Jacksondg712@users.noreply.github.com>
2025-06-18 14:44:40 -0500

    plantilla

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello, React!</h1>
+      <p>Your app is running correctly.</p>
+    </div>
+  );
+}
+
+export default App;
